e454fb663fde9cab66b169f33d256c9e85ee55c7
ArmineDemirchyan <100238894+ArmineDemirchyan@users.noreply.github.com>
2022-09-08 12:11:11 +0400

    ToDoList with classComponent finished

---
+++ src/ClassComponent.js
@@ -0,0 +1,75 @@
+import {Component} from "react";
+
+class ClassComponent extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      newItem:"",
+      list:[]
+    }
+  }
+  updateInput(key, value){
+    this.setState({
+      [key]: value
+    })
+  }
+  addItem(){
+    const newItem = {
+      id:1 + Math.random(),
+      value: this.state.newItem.slice()
+    }
+    const list = [...this.state.list];
+    list.push(newItem)
+
+    this.setState({
+      list,
+      newItem:""
+    });
+  }
+  deletItem(id){
+    const list = [...this.state.list];
+    const updatedList = list.filter(xndzor => xndzor.id!== id);
+    this.setState({list: updatedList})
+  }
+  render(){
+    return(
+      <div className="App">
+        <h1 className="app-title">My List</h1>
+        <div className="container">
+        <div
+         style={{
+            padding: 30,
+            textAlign: "left",
+            maxWidth: 500,
+            margin: "auto"
+          }}
+        >
+        Add an Item...
+          <br/>
+          <input
+            type = "text"
+            placeholder = "Type item here..."
+            value = {this.state.newItem}
+            onChange = {e => this.updateInput("newItem", e.target.value)}
+          />
+          <button className="add-btn btn-floating"  onClick = {() => this.addItem()}> <i class="material-icons"> + </i></button>
+          <br /> <br />
+          <ul>
+            {this.state.list.map(item => {
+              return(
+                <li key={item.id}>
+                  {item.value}
+                  <button className="btn btn-floating" onClick={() => this.deletItem(item.id)}><i class="material-icons">x</i></button>
+                </li>
+               
+              )             
+            })}
+          </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+ 
+}
+export default ClassComponent;
