ccd8d54ddccec16c7e29cc28dbf77df8308c56ab
ArmineDemirchyan <100238894+ArmineDemirchyan@users.noreply.github.com>
2022-09-08 12:33:24 +0400

    merged App.js and index.js conflicts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-
-function App() {
-  return (
-    <div className="App">
-      Hello World!
-=======
 import React from 'react'
 import  "./App.css"
 import ClassComponent from './ClassComponent'
@@ -14,7 +7,6 @@ function App() {
   return (
     <div>
       <ClassComponent/>
->>>>>>> 01bef45db07c54cf3af2806116c15117cc054aff
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-<<<<<<< HEAD
-=======
-
->>>>>>> 01bef45db07c54cf3af2806116c15117cc054aff
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,8 +8,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 01bef45db07c54cf3af2806116c15117cc054aff
