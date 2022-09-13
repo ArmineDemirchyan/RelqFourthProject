b085cf6327f1d9590811ac5096a9d627454b393c
ArmineDemirchyan <100238894+ArmineDemirchyan@users.noreply.github.com>
2022-09-13 10:55:18 +0400

    some changes

--- src/ClassComponent/ClassComponent.js
+++ src/ClassComponent/ClassComponent.js
@@ -1,24 +1,24 @@
-import {Component} from "react";
+import { Component } from "react";
 import ToDoVideoBg from "../assets/ToDoBg.mp4";
-import {FaBackspace} from 'react-icons/fa';
+import { FaBackspace } from 'react-icons/fa';
 import { Link } from "react-router-dom";
 
-class ClassComponent extends Component{
-  constructor(props){
+class ClassComponent extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-      newItem:"",
-      list:[]
+      newItem: "",
+      list: []
     }
   }
-  updateInput(key, value){
-    this.setState({
-      [key]: value
-    })
-  }
-  addItem(){
+  addItem() {
+
+    if (!this.state.newItem) {
+      alert("Enter an item.");
+      return;
+    }
     const newItem = {
-      id:1 + Math.random(),
+      id: 1 + Math.random(),
       value: this.state.newItem.slice()
     }
     const list = [...this.state.list];
@@ -26,53 +26,58 @@ class ClassComponent extends Component{
 
     this.setState({
       list,
-      newItem:""
+      newItem: ""
     });
   }
-  deletItem(id){
+  deletItem(id) {
     const list = [...this.state.list];
-    const updatedList = list.filter(xndzor => xndzor.id!== id);
-    this.setState({list: updatedList})
+    const updatedList = list.filter(item => item.id !== id);
+    this.setState({ list: updatedList })
   }
-  render(){
-    return(
+  updateInput(key, value) {
+    this.setState({
+      [key]: value
+    })
+  }
+  render() {
+    return (
       <div className="main">
-        <div className="overlay"></div>       
-        <video src={ToDoVideoBg} autoPlay loop muted/>
-        
+        <div className="overlay"></div>
+        <video src={ToDoVideoBg} autoPlay loop muted />
+
         <div className="list_div">
-        <Link to="/" className="go_main_page_arrow"><FaBackspace/></Link>
-        
-        <h1 className="app-title">To_Do_List with ClassComponent</h1>
+          <Link to="/" className="go_main_page_arrow"><FaBackspace /></Link>
+
+          <h1 className="app-title">To_Do_List with ClassComponent</h1>
           <input
             className="toDo_input"
-            type = "text"
-            placeholder = "Type item here..."
-            value = {this.state.newItem}
-            onChange = {e => this.updateInput("newItem", e.target.value)}
+            type="text"
+            placeholder="Type item here..."
+            value={this.state.newItem}
+            onChange={e => this.updateInput("newItem", e.target.value)}
           />
-          <button className="add_button"  onClick = {() => this.addItem()}> <i class="material-icons"> + </i></button>
+          <button className="add_button" onClick={() => this.addItem()}> <i class="material-icons"> + </i></button>
           <br /> <br />
           <ul>
             {this.state.list.map(item => {
-              return(
-                
+              return (
+
                 <li key={item.id}>
                   {item.value}
                   <button onClick={() => this.deletItem(item.id)}><i class="material-icons">x</i></button>
                 </li>
-               
-              )             
+
+              )
             })}
           </ul>
-          
+
         </div>
-    </div>
-      
-        
-      
+      </div>
+
+
+
     )
   }
- 
+
 }
 export default ClassComponent;
--- src/Hooks/ToDoList_Hooks.js
+++ src/Hooks/ToDoList_Hooks.js
@@ -1,62 +1,62 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import ToDoSmile from "../assets/ToDoSmile.mp4";
-import {FaBackspace} from 'react-icons/fa';
+import { FaBackspace } from 'react-icons/fa';
 import { Link } from "react-router-dom";
 
 function ToDoList_Hooks() {
     const [newItem, setNewItem] = useState("")
-    const [list, setList] =useState([])
+    const [list, setList] = useState([])
 
-    function addItem(){
+    function addItem() {
 
-        if(!newItem){
+        if (!newItem) {
             alert("Enter an item.");
             return;
         }
         const item = {
             id: Math.floor(Math.random() * 1000),
-            value:newItem
+            value: newItem
         }
         setList(oldList => [...oldList, item])
         setNewItem("");
         console.log(list);
     }
-    function deleteItem(id){
+    function deleteItem(id) {
         const newList = list.filter(item => item.id !== id);
         setList(newList);
     }
-  return (
-    <div>
-        <div className="main">
-        <div className="overlay"></div>   
-        <video src={ToDoSmile} autoPlay loop muted/>
-        <div className="list_div">
-        <Link to="/" className="go_main_page_arrow"><FaBackspace/></Link>
-        <h1 className="app-title">To_Do_List with Hooks</h1>
-        
-        <input
-            className="toDo_input"
-            placeholder="Add an item..."
-            value={newItem}
-            onChange ={e => setNewItem(e.target.value)}
-        />
+    return (
+        <div>
+            <div className="main">
+                <div className="overlay"></div>
+                <video src={ToDoSmile} autoPlay loop muted />
+                <div className="list_div">
+                    <Link to="/" className="go_main_page_arrow"><FaBackspace /></Link>
+                    <h1 className="app-title">To_Do_List with Hooks</h1>
 
-        <button className="add_button" onClick={() => addItem()}>Add</button>
-        
-        <ul>
-             {list.map(item =>{
-               return (               
-                <li key={item.id}>{item.value} 
-                <button onClick={() => deleteItem(item.id)}><i class="material-icons">x</i></button>
-                </li>              
-               )
-            })} 
-        </ul> 
+                    <input
+                        className="toDo_input"
+                        placeholder="Add an item..."
+                        value={newItem}
+                        onChange={e => setNewItem(e.target.value)}
+                    />
+
+                    <button className="add_button" onClick={() => addItem()}>Add</button>
+
+                    <ul>
+                        {list.map(item => {
+                            return (
+                                <li key={item.id}>{item.value}
+                                    <button onClick={() => deleteItem(item.id)}><i class="material-icons">x</i></button>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            </div>
         </div>
-    </div>
-    </div>
-    
-  )
+
+    )
 }
 
 export default ToDoList_Hooks
